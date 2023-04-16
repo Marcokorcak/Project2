c07b3cb3633a5056294b53548f418eed0393f957
Marco Korcak <marcokorcak@Marcos-MacBook-Air.local>
2023-04-15 21:20:49 -0400

    added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./Pages/Home";
 import Create from "./Pages/Create";
-import "./App.css";
+import './index.css';
 
 function App() {
   return (
