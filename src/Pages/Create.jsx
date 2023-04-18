9b09bb8b9eba847a736d8faba710350ce3338ceb
Marco Korcak <marcokorcak@Marcos-MacBook-Air.local>
2023-04-17 22:52:03 -0400

    added button

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -4,6 +4,7 @@ import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import axios from 'axios';
 import ErrorAlert from '../components/ErrorAlert';
+import { Link } from 'react-router-dom';
 
 const Create = () => {
 	const [title, setTitle] = useState('');
@@ -109,6 +110,35 @@ const Create = () => {
 								</span>
 							</a>
 						</div>
+						<Link to={'/posts'}>
+							<div className="edit_buttonv2">
+								<a
+									href="#_"
+									className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-orange-500 rounded-full shadow-md group"
+								>
+									<span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-orange-500 group-hover:translate-x-0 ease">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth="1.5"
+											stroke="currentColor"
+											className="w-6 h-6"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+											/>
+										</svg>
+									</span>
+									<span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">
+										Return Home
+									</span>
+									<span className="relative invisible">Return Home</span>
+								</a>
+							</div>
+						</Link>
 					</div>
 				</>
 			)}
